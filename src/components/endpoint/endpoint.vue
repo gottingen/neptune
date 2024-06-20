6ea8d52585dcc6b9a81a2a8209697f04785172d6
jeff.li <lijippy@163.com>
2024-06-21 02:35:47 +0800

    kv

---
+++ src/components/endpoint/endpoint.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+let server_endpoint = "localhost:8018";
+
+</script>
+
+<template>
+
+  <el-input v-model="server_endpoint" placeholder="请输入服务器地址" style="width: 100%;"></el-input>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
